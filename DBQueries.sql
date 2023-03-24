e222039e11ffaf5dea2132107bff6f2fbac1a052
Joseph Driedger <jdriedger3@my.bcit.ca>
2023-03-24 14:13:21 -0700

    Added layout of Questions that need to be addressed in list of queries in DBQueries.sql

---
+++ DBQueries.sql
@@ -0,0 +1,37 @@
+-- Number of Hours Each Employee Works per Week
+
+-- Number of labour hours last week
+
+-- List of Employees that worked in breaker shifts last month
+
+-- How many breaker shifts are scheduled this week
+
+-- How many slot attendants are scheduled today
+
+-- Any slot attendants not assigned to Section NORTH in last month
+
+-- Number of active Written Warning (WW) employee A has
+
+-- List of active WW sorted by date and employee
+
+-- List of employee's discipline/performance actions
+
+-- Number of sick days an employee has available
+
+-- Number of vacation days an employee has available
+
+-- Employee Stats
+
+---- Num of Females
+---- Num of Males
+---- Average Age of Employees
+---- Num of Employees Over 50
+---- Num of Employees Under 30
+
+-- List of Employees that have mandatory certification expiring in next 6 weeks
+
+-- List of Employees that need updated in-house training
+
+-- List of employees who have expired training
+
+-- Num of unallocated uniforms
